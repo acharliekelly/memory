5700b46be6bd96dcb73507ab52f70098398f990a
Charlie Kelly <charliekelly31415@gmail.com>
2019-01-21 16:17:10 -0500

    made everything better

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,59 +1,182 @@
 
-var deck = ["queen-of-diamonds", "queen-of-hearts", "king-of-diamonds", "king-of-hearts"];
+
+var deck = ["jack-diamonds","queen-diamonds","king-diamonds","ace-diamonds",
+			"jack-clubs","queen-clubs","king-clubs","ace-clubs",
+			"jack-hearts","queen-hearts", "king-hearts","ace-hearts",
+			"jack-spades","queen-spades","king-spades","ace-spades"];
 var cardsInPlay = [];
 
 
-var createBoard = function() {
-	for (var i=0; i<deck.length; i++) {
-		var cardElement = document.createElement('img');
-		cardElement.setAttribute('src', "images/back.png");
-		cardElement.setAttribute('data-id', i);
-		cardElement.setAttribute('data-name', deck[i]);
-		cardElement.addEventListener('click', flipCard);
-		document.getElementById('game-board').appendChild(cardElement);
-	}
-}
 
+
+/**
+ * flip a card over
+ */
 var flipCard = function() {
-	var src = "images/" + this.getAttribute('data-name') + ".png";
+	var src = "images/" + this.getAttribute('data-name') + ".jpg";
 	this.setAttribute('src', src);
+	this.className = 'flipped';
 	cardsInPlay.push(this);
-	checkWin();
+	checkMatch();
 }
 
-var getRank = function(cardElement) {
-	var cardName = cardElement.getAttribute('data-name');
-	var n = cardName.indexOf("-");
-	return cardName.substring(0, n);
-}
-
-var checkWin = function() {
+/**
+ * check if flipped cards match
+ */
+var checkMatch = function() {
 	if (cardsInPlay.length === 2) {
-		if (getRank(cardsInPlay[0]) == getRank(cardsInPlay[1])) {
-			// you win
-			setTimeout(function(){
-				alert("You Win!");
-				resetCards();
-			}, 1000);
+		var rank1 = cardsInPlay[0].getAttribute('data-rank');
+		var rank2 = cardsInPlay[1].getAttribute('data-rank');
+		if (rank1 == rank2) {
+			// match found
+			setTimeout(matchFound, 1000);
 			
 		} else {
-			// you lose
-			setTimeout(resetCards, 2000);
-
+			// no match
+			setTimeout(noMatch, 1000);
 		}
 		
 	} 
-	// otherwise do nothing
+	
+	if (isWin()) {
+		
+		var matchElem = document.querySelector("#match");
+		matchElem.className = 'show';
+		matchElem.innerHTML = "YOU WIN!!"
+
+	}
 }
 
-var resetCards = function() {
-	document.querySelectorAll("#game-board img").forEach(function(img){
-		img.setAttribute('src', 'images/back.png');
 
-	});
-	cardsInPlay = [];
+/**
+ * check if win condition is met
+ */
+var isWin = function() {
+	var foundCards = document.querySelectorAll("img.matched");
+	return (foundCards.length == deck.length);
 }
 
-createBoard();
+/**
+ * turn cards back over after failing to find match
+ */
+var noMatch = function() {
+	matchCondition(false);
+}
+
+/**
+ * let player know that match was found
+ */
+var matchFound = function() {
+	matchCondition(true);
+}
+
+/**
+ * change board based on whether match was found
+ */
+var matchCondition = function(boolMatch) {
+	// only show if found - otherwise just annoying
+	if (boolMatch)
+		showMatchMessage(boolMatch);
+
+	setTimeout(function(){
+		hideMatchMessage();
+		clearInPlay(boolMatch);
+	}, 1000);
+}
+
+var clearInPlay = function(boolMatch) {
+	while (cardsInPlay.length > 0) {
+		var cardElement = cardsInPlay.pop();
+		if (boolMatch) {
+			// match was found
+			cardElement.classList.add('matched');
+			cardElement.removeEventListener('click', flipCard);
+		} else {
+			// no match
+			cardElement.classList.remove('flipped');
+			cardElement.setAttribute('src', 'images/back.jpg');
+		}
+	}
+}
+
+/**
+ * show message indicating match found
+ */
+var showMatchMessage = function(boolMatch) {
+	var matchElem = document.querySelector("#match");
+	matchElem.className = 'show';
+	setTimeout(hideMatchMessage, 2000);
+	matchElem.innerHTML = boolMatch ? "Match Found!" : "No Match";
+}
+
+var hideMatchMessage = function() {
+	var matchElem = document.querySelector("#match");
+	matchElem.className = '';
+	matchElem.innerHTML = 'No Match';
+}
+
+/**
+ * re-create the board
+ */
+var resetBoard = function() {
+	document.getElementById('game-board').innerHTML = "";
+	createBoard();
+}
+
+/**
+ * shuffle the deck
+ */
+var shuffle = function() {
+	// based on Fisher-Yates algorithm
+	var currentIndex = deck.length;
+	var temporaryValue, randomIndex;
+
+	// while there are elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// swap it with current element
+		temporaryValue = deck[currentIndex];
+		deck[currentIndex] = deck[randomIndex];
+		deck[randomIndex] = temporaryValue;
+	}
+}
+
+
+/**
+ * initialize the board
+ */
+var createBoard = function() {
+	shuffle();
+	for (var i=0; i<deck.length; i++) {
+		var cardName = deck[i];
+		var dash = cardName.indexOf('-');
+		var rank = cardName.substring(0,dash);
+		var suit = cardName.substring(dash+1);
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.jpg");
+		cardElement.setAttribute('data-id', i);
+		cardElement.setAttribute('data-name', cardName);
+		cardElement.setAttribute('data-rank', rank);
+		cardElement.setAttribute('data-suit', suit);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	
+	
+}
+
+
+var init = function() {
+	var $resetBtn = document.querySelector('#resetBtn');
+	$resetBtn.addEventListener('click', resetBoard);
+
+	createBoard();
+}
+
+init();
 
 
